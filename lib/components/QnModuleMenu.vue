8e847fc9ac16c9aecdbe6eb651405fe0da4cf774
qmw <123349527@qq.com>
2024-10-11 17:29:54 +0800

    init

--- lib/components/QnModuleMenu.vue
+++ lib/components/QnModuleMenu.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { NLayoutSider, NMenu } from 'naive-ui'
+import { computed } from 'vue'
 
 export interface Option {
   id: number | string
@@ -9,18 +10,38 @@ export interface Option {
 }
 
 export interface ModuleMenuProps {
-  value: number | string
-  options: []
+  moduleId: number | string
+  menuId: number | string
+  options: Option[]
 }
 
-
+const props = withDefaults(defineProps<ModuleMenuProps>(), {
+  moduleId: '',
+  menuId: '',
+  options: [],
+})
+const leftOptions = computed(() => props.options.filter(e => !e.parentId))
+const rightOptions = computed(() => props.options.filter(e => e.parentId === props.moduleId))
 
 </script>
 <template>
-  <n-layout-sider :native-scrollbar="false">
-    <n-menu></n-menu>
+  <!-- 左侧模块 -->
+  <n-layout-sider :native-scrollbar="false"
+                  :width="80"
+                  inverted bordered
+  >
+    <n-menu :value="moduleId"
+            :options="leftOptions"
+    ></n-menu>
   </n-layout-sider>
-  <n-layout-sider :native-scrollbar="false">
-    <n-menu :value="value"></n-menu>
+  <!-- 右侧菜单 -->
+  <n-layout-sider :native-scrollbar="false"
+                  :width="170"
+                  bordered
+                  collapse-mode="width"
+  >
+    <n-menu :value="menuId"
+            :options="rightOptions"
+    ></n-menu>
   </n-layout-sider>
 </template>
