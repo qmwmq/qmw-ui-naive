9abd269e725afa183c1837b53ecacd81097d1684
qmw <123349527@qq.com>
2024-10-16 17:31:08 +0800

    init

---
+++ lib/components/QnDrawer.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { NDrawer, NDrawerContent } from 'naive-ui'
+
+withDefaults(defineProps<{
+  visible: boolean
+  loading: boolean
+  width: number
+  header?: string | number
+}>(), {
+  visible: false,
+  loading: false,
+  width: 512
+})
+
+defineEmits([ 'update:visible' ])
+</script>
+<template>
+  <n-drawer :show="visible"
+            @update:show="$emit('update:visible', $event)"
+            :default-width="width"
+            :auto-focus="false"
+            resizable
+  >
+    <n-drawer-content :native-scrollbar="false" closable>
+
+      <template #header></template>
+
+      <slot></slot>
+
+      <template #footer></template>
+
+    </n-drawer-content>
+  </n-drawer>
+</template>
