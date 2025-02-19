bae05a36f5b4ed69d89f78fdced036edb6d22244
qmw <123349527@qq.com>
2025-02-19 18:03:32 +0800

    1.0.10 1.导出type:Tab 2.升级依赖

--- lib/index.ts
+++ lib/index.ts
@@ -25,6 +25,7 @@ export { default as theme } from './themes'
 
 export type { DataTableColumn } from './components/QnDataTable.vue'
 export type { Option as MenuOption } from './components/QnModuleMenu.vue'
+export type { Tab } from './components/QnTabs.vue'
 
 export {
   QnButton,
