30e05093ad009996611e4efc2c4d5dbc675a1422
qmw <123349527@qq.com>
2025-02-23 19:22:17 +0800

    增加QnSegment

---
+++ lib/components/QnSegment.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { NTabs, useThemeVars } from 'naive-ui'
+
+const themeVars = useThemeVars()
+</script>
+<template>
+  <n-tabs type="segment">
+    <slot></slot>
+  </n-tabs>
+</template>
+<style scoped>
+:deep(.n-tabs-capsule) {
+  background: v-bind('themeVars.primaryColor') !important;
+}
+
+@keyframes text-color {
+  to {
+    color: white;
+  }
+}
+
+:deep(.n-tabs-tab.n-tabs-tab--active .n-tabs-tab__label) {
+  animation: text-color 0.2s linear;
+  animation-fill-mode: forwards;
+}
+
+:deep(.n-tabs-rail) {
+  padding: 2px;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <qn-config-provider theme="light">
     <n-theme-editor>
-      <modal-view></modal-view>
+      <qn-segment>
+        <n-tab name="1" tab="xxx"></n-tab>
+        <n-tab name="2" tab="aaa"></n-tab>
+      </qn-segment>
+<!--      <modal-view></modal-view>-->
       <select-view></select-view>
       <date-picker-view></date-picker-view>
       <data-table-view></data-table-view>
@@ -10,12 +14,13 @@
   </qn-config-provider>
 </template>
 <script setup lang="tsx">
+import {NTab} from 'naive-ui'
 import { NThemeEditor } from 'naive-ui'
 import { QnConfigProvider } from '../lib'
 import MenuView from './test-view/MenuView.vue'
 import DataTableView from './test-view/DataTableView.vue'
 import DatePickerView from './test-view/DatePickerView.vue'
 import SelectView from './test-view/SelectView.vue'
-import UploadTest from './test-view/UploadTest.vue'
 import ModalView from './test-view/ModalView.vue'
+import QnSegment from '../lib/components/QnSegment.vue'
 </script>
